1b988c63880609fdcac568aa82c0488adbbd5848
Andrew Carlisle <ohmygodadinosaur@gmail.com>
2019-08-14 23:45:16 +0000

    Added user authentication to the destroy action. Now only logged in users that created a place can destroy it.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   # Ensures a user is logged in before adding a new location
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(page: params[:page], per_page: 10)
@@ -39,6 +39,10 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+    
     @place.destroy
     redirect_to root_path
   end
